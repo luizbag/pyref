cce7cc35e35d07799938d1cfae4a90f3bef3887f
Luiz Gavioli <luizbag@gmail.com>
2016-03-20 20:10:22 -0300

    Creating a class

--- pyref.py
+++ pyref.py
@@ -2,9 +2,19 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-builder = Gtk.Builder()
-builder.add_from_file("layout.glade")
-win = builder.get_object("applicationwindow1");
-win.connect("delete-event", Gtk.main_quit)
-win.show_all()
-Gtk.main()
+class MainWindow(object):
+	def __init__(self):
+		builder = Gtk.Builder()
+		builder.add_from_file("screens/layout.glade")
+		builder = Gtk.Builder()
+		builder.add_from_file("screens/layout.glade")
+		win = builder.get_object("applicationwindow1")
+		win.connect("delete-event", Gtk.main_quit)
+		win.connect("activate", self.about_activated);
+		win.show_all()
+		Gtk.main()
+
+	def about_activated(self, widget):
+		pass
+
+MainWindow()
