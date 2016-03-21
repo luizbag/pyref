add45695fa51c4c1a50477afd2b5abf6ed0ac6af
Luiz Gavioli <luizbag@gmail.com>
2016-03-20 22:28:11 -0300

    Menu events

--- pyref.py
+++ pyref.py
@@ -8,17 +8,35 @@ class MainWindow(object):
 		builder = Gtk.Builder()
 		builder.add_from_file("layout.glade")
 		builder.connect_signals({
+			"on_novo_menu_item_activate": self.on_novo_menu_item_activate,
+			"on_abrir_menu_item_activate": self.on_abrir_menu_item_activate,
+			"on_salvar_menu_item_activate": self.on_salvar_menu_item_activate,
+			"on_salvar_como_menu_item_activate": self.on_salvar_como_menu_item_activate,
 			"on_aboutMenuItem_activate": self.on_aboutMenuItem_activate,
+			"on_sair_menu_item_activate": Gtk.main_quit,
 			"on_main_window_delete_event": Gtk.main_quit
 		})
 		self.about = builder.get_object("about_dialog")
+		self.file_chooser = builder.get_object("file_chooser_dialog")
 		self.win = builder.get_object("main_window")
 		self.win.show_all()
+
+	def on_novo_menu_item_activate(self, widget):
+		pass
+
+	def on_abrir_menu_item_activate(self, widget):
+		self.file_chooser.run()
+
+	def on_salvar_menu_item_activate(self, widget):
+		pass
+
+	def on_salvar_como_menu_item_activate(self, widget):
+		pass
 
 	def on_aboutMenuItem_activate(self, widget):
 		self.about.run()
 		self.about.hide()
 
 if __name__ == "__main__":
-    app = MainWindow();
+    app = MainWindow()
     Gtk.main()
