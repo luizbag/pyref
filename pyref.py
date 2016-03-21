461f0b42bf5e7513363926be17d125598e6c57cd
Luiz Gavioli <luizbag@gmail.com>
2016-03-20 21:55:03 -0300

    Main Window

--- pyref.py
+++ pyref.py
@@ -3,18 +3,22 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 class MainWindow(object):
+
 	def __init__(self):
 		builder = Gtk.Builder()
-		builder.add_from_file("screens/layout.glade")
-		builder = Gtk.Builder()
-		builder.add_from_file("screens/layout.glade")
-		win = builder.get_object("applicationwindow1")
-		win.connect("delete-event", Gtk.main_quit)
-		win.connect("activate", self.about_activated);
-		win.show_all()
-		Gtk.main()
+		builder.add_from_file("layout.glade")
+		builder.connect_signals({
+			"on_aboutMenuItem_activate": self.on_aboutMenuItem_activate,
+			"on_main_window_delete_event": Gtk.main_quit
+		})
+		self.about = builder.get_object("about_dialog")
+		self.win = builder.get_object("main_window")
+		self.win.show_all()
 
-	def about_activated(self, widget):
-		pass
+	def on_aboutMenuItem_activate(self, widget):
+		self.about.run()
+		self.about.hide()
 
-MainWindow()
+if __name__ == "__main__":
+    app = MainWindow();
+    Gtk.main()
