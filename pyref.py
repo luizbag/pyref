5fe3d7e5a6c22baad9801d4fa03a0901f1692298
Luiz Gavioli <luizbag@gmail.com>
2016-03-25 16:05:37 -0300

    Ajeitando o tamanho da tela

--- pyref.py
+++ pyref.py
@@ -5,6 +5,8 @@ from gi.repository import Gtk
 import models
 
 class MainWindow(object):
+        filename = ''
+        
 
         def __init__(self):
                 builder = Gtk.Builder()
@@ -12,8 +14,6 @@ class MainWindow(object):
                 builder.connect_signals({
                         "on_novo_menu_item_activate": self.on_novo_menu_item_activate,
                         "on_abrir_menu_item_activate": self.on_abrir_menu_item_activate,
-                        "on_salvar_menu_item_activate": self.on_salvar_menu_item_activate,
-                        "on_salvar_como_menu_item_activate": self.on_salvar_como_menu_item_activate,
                         "on_fechar_arquivo_menu_item_activate": self.on_fechar_arquivo_menu_item_activate,
                         "on_aboutMenuItem_activate": self.on_aboutMenuItem_activate,
                         "on_sair_menu_item_activate": Gtk.main_quit,
@@ -22,30 +22,28 @@ class MainWindow(object):
 		self.about = builder.get_object("about_dialog")
                 self.file_chooser = builder.get_object("file_chooser_dialog")
                 self.win = builder.get_object("main_window")
+                self.win.set_default_size(800, 600)
                 self.win.show_all()
 
 	def on_novo_menu_item_activate(self, widget):
                 self.file_chooser.set_action(Gtk.FileChooserAction.SAVE)
                 response = self.file_chooser.run()
                 if response == Gtk.ResponseType.OK:
-                        models.init(self.file_chooser.get_filename())
+                        self.filename = self.file_chooser.get_filename()
+                        models.init(self.filename)
                         models.open()
                         models.create_tables()
-                self.file_chooser.hide()
+                        self.file_chooser.hide()
 
 	def on_abrir_menu_item_activate(self, widget):
                 self.file_chooser.set_action(Gtk.FileChooserAction.OPEN)
                 response = self.file_chooser.run()
                 if response == Gtk.ResponseType.OK:
-                        models.init('database.db')
-                self.file_chooser.hide()
-
-	def on_salvar_menu_item_activate(self, widget):
-                pass
-
-	def on_salvar_como_menu_item_activate(self, widget):
-                pass
-        
+                        self.filename = self.file_chooser.get_filename()
+                        models.init(self.filename)
+                        models.open()
+                        self.file_chooser.hide()
+                
         def on_fechar_arquivo_menu_item_activate(self, widget):
                 models.close()
 
